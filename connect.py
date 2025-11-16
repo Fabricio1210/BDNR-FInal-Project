d14e5f399e13e54543d3a82cca62ba848941614f
Fabricio1210 <fabriciodlvalencia@gmail.com>
2025-11-15 22:47:03 -0600

    h

--- connect.py
+++ connect.py
@@ -1,10 +1,15 @@
 """
 Connections to the databases
 """
-from Cassandra.cassandra import CassandraService
+#from Cassandra.cassandra import CassandraService
+from Mongo.Mongo import MongoSingleton
 
 def main():
     """
     No docstring >:(
     """
-    CassandraService()
+    #CassandraService()
+    mongo_instance = MongoSingleton()
+
+if __name__ == "__main__":
+    main()
